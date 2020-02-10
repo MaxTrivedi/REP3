18395247361dedc4826ff56ba40b41b31225e583
Max Trivedi <maxtrivedi@icloud.com>
2020-02-10 15:30:57 +0000

    Add user input

--- Bond_Differential_IAV.py
+++ Bond_Differential_IAV.py
@@ -2,22 +2,45 @@ import numpy as np
 from scipy.integrate import odeint
 import matplotlib.pyplot as plt
 
+Par_Value = float(input("Enter initial price of the bond:"))
+print("£" + str(Par_Value))
+if Par_Value == 0:
+    Par_Value = 10000
+    Capital_Increase = 10000
+    Bond_Duration = 10
+    Bond_Interest = 0.08
+else:
+    Capital_Increase = float(input("Enter yearly capital increase:"))
+
+    Bond_Duration = int(input("Enter the duration of the bond(years):"))
+    print(str(Bond_Duration)+" years")
+
+    Bond_Interest = float(input("Enter interest rate of the bond(as %):"))
+    Bond_Interest = Bond_Interest/100
+
+
+
+
 # function that returns ds/dt
 def model(y,t):
-    k = 0
-    r = 0.04
+    global Capital_Increase
+    global Bond_Interest
+    k = Capital_Increase
+    r = Bond_Interest
     dydt = k + r*y
     return dydt
 
 def modelIAV(y,t):
-    k = 0
-    r = 0.04
+    global Capital_Increase
+    global Bond_Interest
+    k = Capital_Increase
+    r = Bond_Interest
     i = 0.02888
     dydt = k + r*y - i*y
     return dydt
 
 # initial condition
-y0 = 1000
+y0 = Par_Value
 
 # time points
 t = np.linspace(0,10)
