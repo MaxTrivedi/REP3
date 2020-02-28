a9f5b9033a06c9507ad6008108eb5dcff54d7d6d
Theo Le Vay <xh19142@it075815.wks.bris.ac.uk>
2020-02-28 16:21:50 +0000

    Making the changes

--- 2nd_Model_ZeroCouponBond_IAV.py
+++ 2nd_Model_ZeroCouponBond_IAV.py
@@ -1,11 +1,14 @@
+import matplotlib.pyplot as plt
 import numpy as np
 from scipy.integrate import odeint
-import matplotlib.pyplot as plt
+
 
 Par_Value = 1000
 Capital_Increase = 100
-Bond_Duration = 10
+Bond_Duration = 10000
 Bond_Interest = 0.04
+i = 0.0321
+
 
 # function that returns ds/dt
 def model(y,t):
@@ -21,26 +24,42 @@ def modelIAV(y,t):
     global Bond_Interest
     k = Capital_Increase
     r = Bond_Interest
-    i = 0.0321
     dydt = k + r*y - i*y
     return dydt
+
 
 # initial condition
 y0 = Par_Value
 
 # time points
-t = np.linspace(0,10)
+t = np.linspace(0,10,100)
 
 # solve ODE
-y = odeint(model,y0,t)
-y2 = odeint(modelIAV,y0,t)
+y = odeint(model, y0, t)
+y2 = odeint(modelIAV, y0, t)
 
 # plot results
+plt.xlim((0,10))
 plt.ylim((0,3000))
-plt.plot(t,y)
-plt.plot(t,y2)
+
+plt.plot(t,y, label ='Not Inflation Adjusted')
+plt.plot(t,y2, label = 'Inflation Adjusted')
+
+plt.legend(loc='lower right')
+
 plt.xlabel('time')
 plt.ylabel('y(t)')
+x = np.linspace(0,10,100)
+
+h1 = plt.hlines(y = 2720, xmin = 0, xmax= 10, linestyles = ':')
+v1 = plt.vlines(x = 10, ymin = 0, ymax = 2720,linestyles = ':')
+
+h2 = plt.hlines(y = 2115, xmin = 0, xmax= 10, linestyles = ':')
+
+
+
+plt.plot(x, y, 'b', label = 'y=')
+plt.grid()
 plt.show()
 
 
